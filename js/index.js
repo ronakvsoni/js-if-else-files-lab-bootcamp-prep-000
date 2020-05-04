fd881db598d1b74c9b7a2ac35f11938cbc85329d
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-04 16:52:52 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,8 +4,7 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-if (name === "Susan") {
-  console.log("The name is correct")
-} else {
+var name = "Susan"
+else {
   console.log("Expected the name to be Susan")
 }
